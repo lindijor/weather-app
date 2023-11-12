aef34b66ae496c8b5e7f0a1bc5ce900e75d39892
lindijor <jorgensenlindis@gmail.com>
2023-11-12 14:09:17 +0100

    Added weather data and date with API

--- src/script.js
+++ src/script.js
@@ -2,9 +2,42 @@ function updateWeatherData(response) {
   let cityTemperature = document.querySelector("#weather-degrees");
   let temperature = response.data.temperature.current;
   let city = document.querySelector("#weather-city");
+  let weatherDescription = document.querySelector("#weather-description");
+  let weatherHumidity = document.querySelector("#weather-humidity");
+  let weatherWind = document.querySelector("#weather-wind");
+  let currentTime = document.querySelector("#current-time");
+  let date = new Date(response.data.time * 1000);
+
+  console.log(response.data);
 
   city.innerHTML = response.data.city;
+  weatherDescription.innerHTML = response.data.condition.description;
+  weatherHumidity.innerHTML = response.data.temperature.humidity;
+  weatherWind.innerHTML = response.data.wind.speed;
+  currentTime.innerHTML = getTheDate(date);
   cityTemperature.innerHTML = Math.round(temperature);
+}
+
+function getTheDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day}, ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
