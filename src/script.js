4ac45a5d1b95905037deeda20f8770b654b36d9a
lindijor <jorgensenlindis@gmail.com>
2023-11-12 13:31:42 +0100

    Added temperature search with API

--- src/script.js
+++ src/script.js
@@ -1,11 +1,26 @@
+function updateWeatherData(response) {
+  let cityTemperature = document.querySelector("#weather-degrees");
+  let temperature = response.data.temperature.current;
+  let city = document.querySelector("#weather-city");
+
+  city.innerHTML = response.data.city;
+  cityTemperature.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "025000aa1bof6148etc27f34c35bd48a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeatherData);
+}
+
 function displayCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let city = document.querySelector("#weather-city");
-  city.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#city-search");
 searchFormElement.addEventListener("submit", displayCity);
 
-axios.get(apiUrl).then(showWeatherData);
+searchCity("Oslo");
