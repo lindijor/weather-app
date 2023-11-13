092a7873413fe0fb9c033dce23050ce2e65e1790
lindijor <jorgensenlindis@gmail.com>
2023-11-13 22:19:24 +0100

    Added forecast data in JS with API

--- src/script.js
+++ src/script.js
@@ -17,6 +17,8 @@ function updateWeatherData(response) {
   weatherWind.innerHTML = response.data.wind.speed;
   currentTime.innerHTML = getTheDate(date);
   cityTemperature.innerHTML = Math.round(temperature);
+
+  getForecast(response.data.city);
 }
 
 function getTheDate(date) {
@@ -52,6 +54,44 @@ function displayCity(event) {
   let searchInput = document.querySelector("#search-input");
 
   searchCity(searchInput.value);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "025000aa1bof6148etc27f34c35bd48a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
+}
+
+function showForecast(response) {
+  console.log(response.data);
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        ` <div>
+        <div class="forecast-day">${formatDay(day.time)}</div>
+        <img src="${day.condition.icon_url}" class="forecast-icon">
+        <div class="forecast-temperature"><strong class="forecast-temperature-max">${Math.round(
+          day.temperature.maximum
+        )}</strong> | <span class="forecast-temperature-min">${Math.round(
+          day.temperature.minimum
+        )}</span></div>
+    </div>
+        `;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
 }
 
 let searchFormElement = document.querySelector("#city-search");
